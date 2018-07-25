9dd7db9e340b88df6503d1192cf446ba7007ccf7
Jonathan <jonathan.comberbach@gmail.com>
2018-07-25 23:07:57 +0000

    added skeleton

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -1,10 +1,10 @@
 exports.seed = function(knex, Promise) {
-  return knex('users').del()
+  return knex('users_example').del()
     .then(function () {
       return Promise.all([
-        knex('users').insert({id: 1, name: 'Alice'}),
-        knex('users').insert({id: 2, name: 'Bob'}),
-        knex('users').insert({id: 3, name: 'Charlie'})
+        knex('users_example').insert({id: 1, name: 'Alice'}),
+        knex('users_example').insert({id: 2, name: 'Bob'}),
+        knex('users_example').insert({id: 3, name: 'Charlie'})
       ]);
     });
 };
