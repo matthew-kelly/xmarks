2e42a4aa2f0d8440ef49fc054cba202a5de3aafa
Jonathan <jonathan.comberbach@gmail.com>
2018-07-28 01:48:33 +0000

    css stylings

--- server.js
+++ server.js
@@ -21,11 +21,12 @@ const mapsRoutes = require("./routes/maps");
 const likesRoutes = require("./routes/likes");
 const pinsRoutes = require("./routes/pins");
 
+
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
-
+app.use(express.static(__dirname + “/views”)); // allows css in expressjs
 // Log knex SQL queries to STDOUT as well
 app.use(knexLogger(knex));
 
